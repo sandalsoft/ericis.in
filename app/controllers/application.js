323273e901bd368d948569c2cb87f675c883e83d
Eric Nelson <eric@sandalsoft.com>
2013-10-20 20:46:23 -0500

    - bigger font - custom font - removed bootstrap

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,7 +1,5 @@
 var ApplicationController = Em.ObjectController.extend({
 	lastUpdated: function() {
-		content.log('fuffffff');
-		return "fuckme";
 		// return moment().format(1382147702);
   }.property(),
 });
