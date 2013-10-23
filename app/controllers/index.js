aa0fc1fc046606bdc48996b95319f4966f941408
Eric Nelson <eric@sandalsoft.com>
2013-10-22 21:52:10 -0500

    - put getNextTo back for adding the 'to [city]'

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,28 +1,36 @@
 var IndexController = Em.ObjectController.extend({
 
-	lastUpdated: function() {
-		return moment.unix(this.get('model.udpated_at')).fromNow();
-	}.property(),
+    lastUpdated: function() {
+        return moment.unix(this.get('model.udpated_at')).fromNow();
+    }.property(),
 
-	nextTripDate: function() {
-		var next_trip_epoch = this.get('model.next_start_date');
-		return moment.unix(next_trip_epoch).format('MMMM Do');
-	}.property(),
+    nextTripDate: function() {
+        var next_trip_epoch = this.get('model.next_start_date');
+        return moment.unix(next_trip_epoch).format('MMMM Do');
+    }.property(),
 
-	nextCity: function() {
-		var next_city = this.get('model.next_city');
-		if (next_city === 'Chicago, IL') {
-			return "home";
-		}
-		else {
-			return next_city;
-		}
-	}.property(),
+    nextCity: function() {
+        var next_city = this.get('model.next_city');
+        if (next_city === 'Chicago, IL') {
+            return "home";
+        } else {
+            return next_city;
+        }
+    }.property(),
 
-	setMarkerToCity: function(lat, lon) {
-			this.set('latitude', lat);
-			this.set('longitude', lon);
-	},
+    setMarkerToCity: function(lat, lon) {
+        this.set('latitude', lat);
+        this.set('longitude', lon);
+    },
+
+    toNextCity: function() {
+        var next_city = this.get('model.next_city');
+        if (next_city === 'Chicago, IL') {
+            return "";
+        } else {
+            return "to ";
+        }
+    }.property(),
 });
 
 export default IndexController;
